cf8bc7bac7ec70556e6a7d2fed33d436a700f68c
hoangkim2792 <hoangkim050702@gmail.com>
2022-08-29 14:53:02 +0700

    1

--- note.js
+++ note.js
@@ -143,3 +143,28 @@ function arrToObj(arr){
 }
 
 console.log(arrToObj(arr)); // { name: 'Sơn Đặng', age: 18 }
+
+// trương trình tính toán sử dụng call back
+
+function sumCb(a, b) {
+    return a+b
+}
+function subCb(a, b) {
+    return a-b
+}
+function multiCb(a, b) {
+ return a*b
+}function divCb(a, b) {
+    return a/b
+}
+function caculate(a, b, cb) {
+    return cb(a, b);
+}
+
+
+
+// Expected results
+// caculate(1, 2, sumCb) // Output: 3
+// caculate(1, 2, subCb) // Output: -1
+// caculate(1, 2, multiCb) // Output: 2
+// caculate(3, 1, divCb) // Output: 3
